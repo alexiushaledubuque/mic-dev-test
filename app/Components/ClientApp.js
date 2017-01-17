98558a9cf476fb0d75cf1e9c6c6c35c862352eb4
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-01-16 22:50:16 -0500

    Modularization

--- app/Components/ClientApp.js
+++ app/Components/ClientApp.js
@@ -1,18 +1,3 @@
-/* global React ReactDOM */
-
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-      div(null,
-        h1({style: { color: this.props.color }}, this.props.title)
-      )
-    )
-  }
-})
-
 var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
